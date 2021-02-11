45a770e88e9ec57d0bf0113bcc3b8812dd0f45ee
Md-Jahid-Hasan <jahid15-1905@gmail.com>
2021-02-12 01:20:52 +0600

    Uploading project without css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react'
+import items from './data'
+
+const other_data = {
+    isFinished: true,
+    scheduleTask: []
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [work, setWork] = useState(items)
+    const [minute, setMinute] = useState('_ _')
+    const [second, setSecond] = useState('_ _')
+    const [schedule, setSchedule] = useState(other_data)
+
+
+    useEffect(() => {
+        console.log("Hello world 2")
+        let timer = null
+        if (second > 0) {
+            timer = setInterval(() => setSecond(second - 1), 1000)
+        } else if (second === 0 && minute > 0) {
+            setMinute(minute - 1)
+            setSecond(60)
+        } else if (second === 0 && minute === 0) {
+            setMinute('_ _')
+            setSecond('_ _')
+            if (schedule.scheduleTask.length > 0) {
+                const task = schedule.scheduleTask.pop()
+                setMinute(work[task].time - 1)
+                setSecond(59)
+            } else {
+                setSchedule({...schedule, isFinished: true})
+            }
+        }
+
+        return () => clearInterval(timer)
+    }, [second])
+
+    const handleButton = (id) => {
+        console.log(id);
+        console.log("Button clicked")
+        if (schedule.isFinished === true) {
+            setMinute(work[id].time - 1)
+            setSecond(59)
+            setSchedule({...schedule, isFinished: false})
+        } else {
+            schedule.scheduleTask.push(id)
+            console.log(schedule.scheduleTask.length)
+        }
+    }
+
+    return (
+        <>
+            <div>
+                {minute} : {second}
+            </div>
+            <div>
+                {schedule.scheduleTask.length ? <p>Upcoming Task</p>:""}
+                {schedule.scheduleTask.map(task => {
+                    const {id, title, time} = work[task];
+                    return (
+
+                        <div key={id}>
+                            <p>{title}</p>
+                            <p>{time}</p>
+                        </div>
+                    )
+                })}
+            </div>
+            <div>
+                {work.map(value => {
+                    const {id, title, time} = value;
+                    return (
+                        <div key={id}>
+                            <p>{title}</p>
+                            <p>{time} min</p>
+                            <button onClick={() => handleButton(id)}>Set time</button>
+                        </div>
+                    )
+                })}
+            </div>
+        </>
+    );
 }
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,24 @@
+const items = [
+    {
+        id: 0,
+        title: 'Small Work',
+        time: 1
+    },
+    {
+        id: 1,
+        title: 'Medium Work',
+        time: 10
+    },
+    {
+        id: 2,
+        title: 'Large Task',
+        time: 20
+    },
+    {
+        id: 3,
+        title: 'Full Time',
+        time: 25
+    }
+];
+
+export default items;
